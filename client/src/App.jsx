cbfbd54f07da7d2e9f2dd3676e1a0e2157c88112
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-05-26 11:55:52 -0600

    finished app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,40 @@
 import './App.css';
+// import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link.context';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <>
+
+// GraphQL 
+const httpLink = createHttpLink({ 
+  uri: '/graphgl',
+});
+
+// Middleware 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// Apollo Client
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+const App = () => {
+  return ( 
+    <ApolloProvider client = {client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
